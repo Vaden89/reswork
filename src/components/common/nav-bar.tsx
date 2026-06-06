b2e6f3e04cf95c60bb6c3e98c43a67a298d2da4f
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-06 03:25:18 +0100

    added actual navigation abilities to the nav items, also added hover animation for nav-links

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -1,12 +1,19 @@
+import { Link } from '@tanstack/react-router'
 import { ProfileCard } from '../user/profile-card'
 
 export const NavBar = () => {
   return (
     <nav className="w-full flex items-center justify-between py-4 border-b  border-b-border px-8 xl:px-0">
-      <h1 className="text-2xl font-medium">Reswork</h1>
+      <Link to="/">
+        <h1 className="text-2xl font-medium">Reswork</h1>
+      </Link>
       <div className="flex items-center gap-4 text-sm font-normal text-secondary">
-        <button>Templates</button>
-        <button>My Resumes</button>
+        <Link to="/builder">
+          <button className="nav-link">Templates</button>
+        </Link>
+        <Link to="/">
+          <button className="nav-link">My Resumes</button>
+        </Link>
       </div>
       <ProfileCard />
     </nav>
