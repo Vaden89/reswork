508f9137b1d6594a5c77ed3247fe7d23c6869ae2
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-06 02:51:42 +0100

    consumed the auth context, by making use of the generated id as the seed thereby stabilzing the avater

--- src/components/user/profile-card.tsx
+++ src/components/user/profile-card.tsx
@@ -1,12 +1,18 @@
+import { useAuth } from '#/context/auth.context'
 import { Navii } from '@usenavii/react'
 
 export const ProfileCard = () => {
-  const uid = crypto.randomUUID()
+  const { user } = useAuth()
 
   return (
     <div className="flex items-center text-sm gap-2">
       <div className="rounded-full">
-        <Navii seed={uid} size={32} title="Guest" className="rounded-full" />
+        <Navii
+          seed={user?.id ?? 'guest'}
+          size={32}
+          title="Guest"
+          className="rounded-full"
+        />
       </div>
     </div>
   )
