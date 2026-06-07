57854a29f52ecfe8affa55db3085c9cffc0aae36
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-08 00:26:40 +0100

    added a list of degree types that can be selected on the educations page

---
+++ src/data/degree-types.ts
@@ -0,0 +1,13 @@
+export const DEGREE_TYPES = [
+  { value: 'BSc', label: 'BSc – Bachelor of Science' },
+  { value: 'BA', label: 'BA – Bachelor of Arts' },
+  { value: 'BEng', label: 'BEng – Bachelor of Engineering' },
+  { value: 'BTech', label: 'BTech – Bachelor of Technology' },
+  { value: 'MSc', label: 'MSc – Master of Science' },
+  { value: 'MA', label: 'MA – Master of Arts' },
+  { value: 'MEng', label: 'MEng – Master of Engineering' },
+  { value: 'MBA', label: 'MBA – Master of Business Administration' },
+  { value: 'PhD', label: 'PhD – Doctor of Philosophy' },
+  { value: 'HND', label: 'HND – Higher National Diploma' },
+  { value: 'OND', label: 'OND – Ordinary National Diploma' },
+]
