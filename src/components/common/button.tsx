cda038424f3c71aec36dbd4cd1900d1172f26fd4
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-08 00:31:43 +0100

    feat: added the button variants allowing for different styles of buttons for different use cases

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -2,24 +2,31 @@ import type { ButtonHTMLAttributes, ReactNode } from 'react'
 import { cn } from '#/utils/cn'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode
   icon?: ReactNode
-  iconPosition?: 'left' | 'right'
   className?: string
+  children: ReactNode
+  variants?: 'default' | 'ghost'
+  iconPosition?: 'left' | 'right'
+}
+
+const variants = {
+  default: 'bg-primary text-white',
+  ghost: 'bg-transparent text-secondary',
 }
 
 export const Button = ({
   children,
   icon,
   iconPosition = 'right',
+  variants: variant = 'default',
   className,
   onClick,
 }: ButtonProps) => {
   const baseStyling =
-    'bg-primary text-white w-fit text-sm font-medium px-2 py-1 flex items-center justify-center gap-2'
+    'w-fit text-sm font-medium px-2 py-1 flex items-center justify-center gap-2'
 
   return (
-    <button onClick={onClick} className={cn(baseStyling, className)}>
+    <button onClick={onClick} className={cn(baseStyling, variants[variant], className)}>
       {iconPosition === 'left' && icon}
       {children}
       {iconPosition === 'right' && icon}
