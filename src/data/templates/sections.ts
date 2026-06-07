7397d9cd4b8be54c97e5a32b4bb63a696967a3e4
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-06-08 00:23:07 +0100

    added a list of resume sections

---
+++ src/data/templates/sections.ts
@@ -0,0 +1,8 @@
+import { BriefcaseBusiness, GraduationCap, Star, UserRound } from 'lucide-react'
+
+export const RESUME_SECTIONS = [
+  { id: 'general', label: 'General Information', icon: UserRound },
+  { id: 'skills', label: 'Skills', icon: Star },
+  { id: 'experience', label: 'Experience', icon: BriefcaseBusiness },
+  { id: 'education', label: 'Education', icon: GraduationCap },
+]
